00c0d9f8685a92db8dd29e789f26647ca8f9888e
MrApocalypse <ensabahnur16@gmail.com>
2014-11-18 13:32:03 -0600

    Add missing sepolicy

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,20 @@
+#
+# This policy configuration will be used by all products that
+# inherit from CM
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/screwd/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    genfs_contexts \
+    seapp_contexts \
+    auditd.te \
+    installd.te \
+    netd.te \
+    system.te \
+    ueventd.te \
+    vold.te \
+    mac_permissions.xml
