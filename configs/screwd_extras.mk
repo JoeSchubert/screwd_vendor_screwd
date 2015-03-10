79e6dd3ae4556530e3fe46a392c26935bf4c6e6a
MrApocalypse <ensabahnur16@gmail.com>
2015-03-10 10:40:15 -0500

    Disable ADB authentication
    For debugging purposes...

--- configs/screwd_extras.mk
+++ configs/screwd_extras.mk
@@ -31,7 +31,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 endif
 
 # Extra packages
