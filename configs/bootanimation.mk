dfc0d60b51dc6c048a88a038f3c4179d8d6a698f
MrApocalypse <ensabahnur16@gmail.com>
2015-05-19 09:31:43 -0500

    Housecleaning...
    -Update copyrights
    -Fix Version in build script
    -Pay the rent

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2014 ParanoidAndroid Project
-# Copyright (C) 2014 Screw'd Android Project
+# Copyright (C) 2015 Screw'd AOSP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
