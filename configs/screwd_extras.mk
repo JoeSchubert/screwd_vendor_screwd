d1bc0c740170806d1776d6262bf7ec6899226cbb
Corey Edwards <ensabahnur16@gmail.com>
2015-06-26 15:40:30 -0500

    Re-Implement Viper4Android
    ...hopefully right this time.
    
    This uses the Material modded version.

--- configs/screwd_extras.mk
+++ configs/screwd_extras.mk
@@ -52,3 +52,29 @@ PRODUCT_PACKAGES += \
 ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
 -include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
 endif
+
+# V4A files
+PRODUCT_COPY_FILES += \
+    vendor/screwd/proprietary/V4A/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    vendor/screwd/proprietary/V4A/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libaudiopreprocessing.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libbundlewrapper.so:system/lib/soundfx/libbundlewrapper.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libdownmix.so:system/lib/soundfx/libdownmix.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libeffectproxy.so:system/lib/soundfx/libeffectproxy.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libldnhncr.so:system/lib/soundfx/libldnhncr.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libqcomvoiceprocessing.so:system/lib/soundfx/libqcomvoiceprocessing.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libreverbwrapper.so:system/lib/soundfx/libreverbwrapper.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libvisualizer.so:system/lib/soundfx/libvisualizer.so \
+    vendor/screwd/proprietary/V4A/priv-app/ViPER4Android/ViPER4Android.apk:system/priv-app/ViPER4Android/ViPER4Android.apk \
+    vendor/screwd/proprietary/V4A/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    
+ # V4A build.prop
+ PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=false \
+    lpa.releaselock=false \
+    lpa.use-stagefright=false \
+    tunnel.decode=false   
+    
+
