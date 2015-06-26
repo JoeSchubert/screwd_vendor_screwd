03d7f1921792667e925128e798bb6ec8e95677fc
Chet Kener <Cl3Kener@gmail.com>
2015-06-26 10:29:21 -0400

    Update N5 Fingerprints to LYZ28E
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- products/screwd_hammerhead.mk
+++ products/screwd_hammerhead.mk
@@ -28,5 +28,5 @@ PRODUCT_NAME := screwd_hammerhead
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.1.1/LMY48B/1863243:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY48B 1863243 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.1.1/LYZ28E/1914015:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LYZ28E 1914015 release-keys"
 endif
