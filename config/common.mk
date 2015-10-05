kn8wolf/vendor_slim

Finisher script (2/2)
Change-Id: I20363713a827629294ffe71f92ebd2d0001026e3

config/common.mk
@@ -204,4 +204,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.slim.buildtype=$(SLIM_BUILD_TYPE)
 
 EXTENDED_POST_PROCESS_PROPS := vendor/slim/tools/slim_process_props.py
+FINISHER_SCRIPT := vendor/slim/tools/finisher
 

JoeSchubert/screwd_vendor_screwd

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

config/common.mk
@@ -188,4 +188,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.screwd.buildtype=$(SCREWD_BUILD_TYPE)
 
 EXTENDED_POST_PROCESS_PROPS := vendor/screwd/tools/screwd_process_props.py
+FINISHER_SCRIPT := vendor/screwd/tools/finisher
 