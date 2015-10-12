0af0de40a6abb5ff72b026648d93836d39ef518e
Corey Edwards <ensabahnur16@gmail.com>
2015-10-12 17:26:27 -0500

    Used UNOFFICIAL SM for HellsCore
    We will switch back to UBER once it's up & running for M
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_hammerhead.mk
+++ products/screwd_hammerhead.mk
@@ -34,6 +34,6 @@ endif
 # Kernel inline build
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := hells_defconfig
-TARGET_GCC_VERSION_ARM := 4.9
+TARGET_GCC_VERSION_ARM := 5.2-SM
 
 
