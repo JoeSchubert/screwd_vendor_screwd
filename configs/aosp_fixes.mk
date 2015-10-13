7cde6c5bcfb460ddeceb07a77c81cc207a6b0c49
Corey Edwards <ensabahnur16@gmail.com>
2015-10-12 20:37:23 -0500

    She used to call me on my cell phone.......
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -21,7 +21,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    net.tethering.noprovisioning=true \
+    drm.service.enabled=true \
+    persist.sys.dun.override=0
 
 # Override old AOSP defaults, we use (new) Google stock sounds
 #PRODUCT_PROPERTY_OVERRIDES += \
