23f5e167b853ab06b14c902136221944187b16e7
Corey Edwards <ensabahnur16@gmail.com>
2015-10-16 16:12:44 -0500

    Fix AOSP Keyboard....maybe
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -40,4 +40,4 @@ PRODUCT_COPY_FILES += \
 
 # Proprietary latinime lib needed for Keyboard swyping
 PRODUCT_COPY_FILES += \
-    vendor/screwd/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+    vendor/screwd/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
