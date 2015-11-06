a4e3ad9ef56be24ff72cbd4d6d13030f49ff66fb
Corey Edwards <ensabahnur16@gmail.com>
2015-11-06 16:44:24 -0600

    shamu: Build w/ UBER GCC 5.2
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_shamu.mk
+++ products/screwd_shamu.mk
@@ -33,7 +33,7 @@ endif
 # Kernel inline build
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
 TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_GCC_VERSION_ARM := 4.9
+TARGET_GCC_VERSION_ARM := 5.2
 
 #Optimize-it!!
 export STRICT_ALIASING := true
