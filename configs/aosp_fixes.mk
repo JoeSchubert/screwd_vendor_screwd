a11718838517312bda6bb399ca4f1f5b7cf1284a
beanstown106 <nbenis106@gmail.com>
2015-12-11 13:53:39 -0500

    aosp_fixes: add missing google error reciever prop
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -20,6 +20,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.setupwizard.network_required=false \
