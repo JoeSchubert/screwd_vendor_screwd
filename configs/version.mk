13ec66fd281f8992201651448f3eeab707b5de68
mydongistiny <jason@oceighty.co>
2016-02-29 13:57:20 -0600

    DTC: Pull version and version to use from vendor (2/2)
    Also see build

--- configs/version.mk
+++ configs/version.mk
@@ -23,3 +23,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.screwd.version=$(SCREWD_VERSION) \
     ro.modversion=$(SCREWD_MOD_VERSION) \
     ro.screwd.buildtype=$(SCREWD_BUILD_TYPE)
+
+# DragonTC info
+DRAGONTC_VERSION := 3.9
+export $(DRAGONTC_VERSION)
+
+DTC_PATH := prebuilts/clang/linux-x86/host/$(DRAGONTC_VERSION)
+DTC_VER := $(shell cat $(DTC_PATH)/VERSION)
+export $(DTC_VER)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.dtc.version=$(DRAGONTC_VERSION)
