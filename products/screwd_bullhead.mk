f07f1d48c5ab97e182b6fec1aa68bd2182f780c6
Omkar <djomi400@gmail.com>
2016-04-01 04:13:58 +0000

    bullhead: compile kernel using 4.9 toolchain

--- products/screwd_bullhead.mk
+++ products/screwd_bullhead.mk
@@ -40,7 +40,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 
 # Inline kernel building
-TARGET_GCC_VERSION_ARM64 := 6.0
+TARGET_GCC_VERSION_ARM64 := 4.9
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := bullhead_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
