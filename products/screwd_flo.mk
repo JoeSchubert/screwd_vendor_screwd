6d370982af6c483c415d055286064414cf944d02
Corey Edwards <ensabahnur16@gmail.com>
2016-08-02 19:46:40 -0500

    Update device fingerprints
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_flo.mk
+++ products/screwd_flo.mk
@@ -37,8 +37,8 @@ PRODUCT_MANUFACTURER := Asus
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
-    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30P/2960889:user/release-keys \
-    PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30P 2960889 release-keys"
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
     
 endif
 
