e0a3776f4e0c16a11ee7145ad02e79128d2189a1
Corey Edwards <ensabahnur16@gmail.com>
2016-09-26 12:46:39 -0500

    angler: Use GCC version 6.x for kernel
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_angler.mk
+++ products/screwd_angler.mk
@@ -42,7 +42,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 
 # Inline kernel building
-TARGET_GCC_VERSION_ARM64 := 4.9
+TARGET_GCC_VERSION_ARM64 := 6.x
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := angler_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
