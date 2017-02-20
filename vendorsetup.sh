6929c4ff9b1fdd796436feaf50d4299049cd1556
Dustin Rinne <dustin.rinne@gmail.com>
2017-02-20 12:45:53 -0600

    Add marlin to the menu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,5 @@ add_lunch_combo screwd_angler-userdebug
 add_lunch_combo screwd_bullhead-userdebug
 add_lunch_combo screwd_flo-userdebug
 add_lunch_combo screwd_hammerhead-userdebug
+add_lunch_combo screwd_marlin-userdebug
 add_lunch_combo screwd_shamu-userdebug
