2668a449939ce0012c18a8eb95a58529198eadf7
Corey Edwards <ensabahnur16@gmail.com>
2017-03-16 08:40:04 -0500

    Add oneplus3
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Fix catastrophic failure!!!
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

---
+++ products/screwd_oneplus3.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2016 Screw'd AOSP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Check for target product
+
+ifeq (screwd_oneplus3,$(TARGET_PRODUCT))
+
+# Include Screw'd common configuration
+include vendor/screwd/main.mk
+
+# Inherit AOSP device configuration for oneplus3
+$(call inherit-product, device/oneplus/oneplus3/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := screwd_oneplus3
+PRODUCT_DEVICE := oneplus3
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR := oneplus
+
+endif
+
+# Inline kernel building
+TARGET_GCC_VERSION_ARM64 := 4.9
+TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
+TARGET_KERNEL_CONFIG := lineageos_oneplus3_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,5 @@ add_lunch_combo screwd_bullhead-userdebug
 add_lunch_combo screwd_flo-userdebug
 add_lunch_combo screwd_hammerhead-userdebug
 add_lunch_combo screwd_marlin-userdebug
+add_lunch_combo screwd_oneplus3-userdebug
 add_lunch_combo screwd_shamu-userdebug
