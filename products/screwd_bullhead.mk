114bac102e0669923f843ab48f502a4e8f5a3977
Corey Edwards <ensabahnur16@gmail.com>
2017-04-10 12:03:38 -0500

    Update shamu & bullhead fingerprint
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_bullhead.mk
+++ products/screwd_bullhead.mk
@@ -35,8 +35,8 @@ endif
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/N4F26T/3687331:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26T 3687331 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.2/N2G47F/3769476:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47F 3769476 release-keys"
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
