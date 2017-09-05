5f09ae73a4f02f1170b223f0314a20b5e3e5b533
Corey Edwards <ensabahnur16@gmail.com>
2017-09-05 13:03:13 -0500

    angler: Update fingerprints to 8.0.0
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_angler.mk
+++ products/screwd_angler.mk
@@ -37,8 +37,8 @@ endif
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G48C/4104010:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G48C 4104010 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:8.0.0/OPR6.170623.013/4283548:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 8.0.0 OPR6.170623.013 4283548 release-keys"
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
