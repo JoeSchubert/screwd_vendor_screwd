70afb1314a810452ee5e4ce056d9afd9ed3c3556
Corey Edwards <ensabahnur16@gmail.com>
2017-09-12 10:03:56 -0500

    hammerhead: come forth...
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 add_lunch_combo screwd_angler-userdebug
 #add_lunch_combo screwd_bullhead-userdebug
 #add_lunch_combo screwd_flo-userdebug
-#add_lunch_combo screwd_hammerhead-userdebug
+add_lunch_combo screwd_hammerhead-userdebug
 add_lunch_combo screwd_marlin-userdebug
 #add_lunch_combo screwd_sailfish-userdebug
 #add_lunch_combo screwd_shamu-userdebug
