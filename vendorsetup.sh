6f368630dba7822d47b42af950c572f87b38ecd1
Paul Tropea <tropeazy@gmail.com>
2017-09-18 02:48:29 -0400

    Shamu, come out to play...

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,4 @@ add_lunch_combo screwd_angler-userdebug
 add_lunch_combo screwd_hammerhead-userdebug
 add_lunch_combo screwd_marlin-userdebug
 #add_lunch_combo screwd_sailfish-userdebug
-#add_lunch_combo screwd_shamu-userdebug
+add_lunch_combo screwd_shamu-userdebug
