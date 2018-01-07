98b60f651cd8e2bb38f7ca5508c672c2f6434078
Corey Edwards <ensabahnur16@gmail.com>
2018-01-07 01:59:20 -0600

    Only show maintained/working devices in list
    The others can still technically be built, but I only want devices that's DD ready in this list.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 add_lunch_combo screwd_angler-userdebug
-add_lunch_combo screwd_bullhead-userdebug
-add_lunch_combo screwd_hammerhead-userdebug
-add_lunch_combo screwd_marlin-userdebug
+#add_lunch_combo screwd_bullhead-userdebug
+#add_lunch_combo screwd_hammerhead-userdebug
+#add_lunch_combo screwd_marlin-userdebug
 #add_lunch_combo screwd_sailfish-userdebug
-add_lunch_combo screwd_shamu-userdebug
+#add_lunch_combo screwd_shamu-userdebug
