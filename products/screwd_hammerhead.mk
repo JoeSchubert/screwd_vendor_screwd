7140b0da5f44f051a870ff42b2adccfd7afe3342
Corey Edwards <ensabahnur16@gmail.com>
2018-01-21 12:26:11 -0600

    hammerhead: Update fingerprints
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/screwd_hammerhead.mk
+++ products/screwd_hammerhead.mk
@@ -34,8 +34,8 @@ endif
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30Y/3067468:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30Y 3067468 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
 
 # Kernel inline build
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
